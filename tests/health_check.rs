ca8c48c54b8f8908cc01be782eb9659375d16c1c
RATIU5 <jcmemmott20@outlook.com>
2023-06-23 14:26:01 -0600

    add: custom port

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -24,7 +24,7 @@ async fn health_check_works() {
 }
 
 fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to bind address");
+    let server = zero2prod::run("127.0.0.1:8000").expect("Failed to bind address");
     // Launch the server as a background task
     // tokio::spawn returns a handle to the spawned future,
     // but we have no use for it here, hence the non-binding let
