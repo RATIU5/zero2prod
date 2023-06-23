fc2e71ad91b3a9e811a059f84568c1d3c7e62730
RATIU5 <jcmemmott20@outlook.com>
2023-06-23 17:30:51 -0600

    add: dynamic port

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    let listener =
+        std::net::TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our Server
-    run("127.0.0.1:8000")?.await
+    run(listener)?.await
 }
