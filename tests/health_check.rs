2841ed78485e2c8d0f6e1cc78131b7db3dc8f7d7
RATIU5 <jcmemmott20@outlook.com>
2023-06-26 09:49:40 -0600

    add: subscriber success test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -27,3 +27,25 @@ fn spawn_app() -> String {
     let _ = tokio::spawn(server);
     format!("http://127.0.0.1:{}", port)
 }
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    // Act
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscriber_returns_a_400_when_datais_missing() {}
